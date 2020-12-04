da0220827fba6090f41e218b091bddfbb1f4933c
애브리띵 <66000432+OHvrything@users.noreply.github.com>
2020-12-04 10:08:46 +0900

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,5 +23,21 @@ async def on_ready():
 @bot.command(name="msg", help="당신이 한 말을 따라합니다.", usage="admin.msg [말]")
 async def Echo(ctx, *, text: str):
     await ctx.send(text)
+    
+@bot.command(name="pf", help="프로필을 보여줍니다!", usage=".pf")
+async def ping(message):
+    embed = discord.Embed(color=0x7289DA, title=f"{message.author} 프로필")
+    embed.set_thumbnail(url=message.author.avatar_url)
+    embed.set_footer(text=message.author, icon_url=message.author.avatar_url)
+    embed.add_field(name="서버 접속일", value=f"{message.author.joined_at.year}년 {message.author.joined_at.month}월 {message.author.joined_at.day}일", inline=False)
+    badge = []
+    if 784034952303607818 in map(lambda x: x.id, message.author.roles):
+        badge.append("<:TEBDEV:784037256242397184>")
+    if 776639464415297576 in map(lambda x: x.id, message.author.roles):
+        badge.append("<:user:784039384416387072>")
+
+    if len(badge) == 0: embed.add_field(name="서버 전용 뱃지", value=f"> **뱃지가 없습니다.**", inline=False)
+    else: embed.add_field(name="서버 전용 뱃지", value=f"> {' '.join(badge)}", inline=False)
+    await message.channel.send(embed=embed)
 
 bot.run(os.environ['token'])
